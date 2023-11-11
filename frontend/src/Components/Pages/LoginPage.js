f4fa606392afd4fd5f4931037bdf150ec57804df
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-11-11 16:11:41 +0100

    style: login page

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -10,16 +10,16 @@ function renderLoginForm() {
     const main = document.querySelector('main');
 
     main.innerHTML = `
-    <div id="formContainer" class="container">
-      <form id="loginForm" class="p-5 text-center text-light">
-        <div id="formTitle" class="bg-warning p-2 rounded mb-4 text-dark">Connectez-vous !</div>
+    <div class="container bg-black p-5 rounded-5 bg-opacity-50 col-3 mt-5">
+      <form class="text-center text-light">
+        <div class="bg-warning p-2 rounded mb-3 text-dark fs-3">Connectez-vous !</div>
 
-        <div class="mb-2">
+        <div>
           <label for="username" class="form-label">Pseudo :</label>
           <input type="text"  id="username" name="username" required>
         </div>
 
-        <div class="mb-3">
+        <div>
           <label for="password" class="form-label">Mot de passe :</label>
           <input type="password"  id="password" name="password" required>
         </div>
