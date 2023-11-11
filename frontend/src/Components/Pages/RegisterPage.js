d7efe39255adb36c6ff1665cddc233284aa5194b
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-11-11 16:03:44 +0100

    style: register page

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -12,19 +12,19 @@ function renderRegisterForm() {
     main.innerHTML = `
     <div class="container bg-black p-5 rounded-5 bg-opacity-50 col-3 mt-5">
       <form class="text-center text-light">
-        <div class="bg-warning p-2 rounded mb-2 text-dark fs-3">S'inscrire</div>
+        <div class="bg-warning p-2 rounded mb-3 text-dark fs-3">S'inscrire</div>
 
-        <div class="">
+        <div>
           <label for="username" class="form-label">Pseudo</label>
           <input type="text"  id="username" name="username" required>
         </div>
 
-        <div class="">
+        <div>
           <label for="password" class="form-label">Mot de passe</label>
           <input type="password"  id="password" name="password" required>
         </div>
 
-        <div class="">
+        <div>
           <label for="comfirm_password" class="form-label">Confirmer le mot de passe</label>
           <input type="password"  id="password" name="comfirm_password" required>
         </div>
