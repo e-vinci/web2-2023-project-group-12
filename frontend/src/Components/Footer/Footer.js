982733c91368133238338b21382fe4ab9cf01082
melda-gonen-vinci <melda.gonen@student.vinci.be>
2023-11-14 12:36:27 +0100

    showing advices in footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,7 +1,26 @@
 const Footer = () => {
   const footer = document.querySelector('footer');
-  footer.classList.add('text-center', 'pt-2', 'bg-warning', 'fixed-bottom');
-  footer.innerHTML = `<h6>Mangez 5 fruits et légumes par jour !</h6>`;
-};
 
+  // api for translating advice
+  async function translateToFrench(text) {
+    const response = await fetch(
+      `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|fr`
+    );
+    const data = await response.json();
+    return data.responseData.translatedText;
+  }
+
+  // api giving random advices
+  fetch('https://api.adviceslip.com/advice')
+    .then((response) => response.json())
+    .then(async (data) => {
+      const originalAdvice = data.slip.advice;
+      const translatedAdvice = await translateToFrench(originalAdvice)
+    footer.classList.add('text-center', 'pt-2', 'bg-warning', 'fixed-bottom');
+    footer.innerHTML = `<h6>${translatedAdvice}</h6>`;
+  }) 
+    .catch((error) => {
+      console.log(error)
+  });
+};
 export default Footer;
