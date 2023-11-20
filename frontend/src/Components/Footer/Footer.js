6c5e18c861679707f831a5e330cf5eff5a538e51
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-11-20 14:40:34 +0100

    add: random advice (footer)

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,31 +1,19 @@
 const footer = document.querySelector('footer');
 const Footer = () => {
   footer.classList.add('text-center', 'pt-2', 'bg-warning', 'fixed-bottom');
-  getRandomQuote();
+  getRandomAdvice();
 };
 
-// api to translate to french
-async function translateToFrench(text) {
-  const response = await fetch(
-    `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|fr`
-  );
-  const data = await response.json();
-  return data.responseData.translatedText;
-};
-
-// api to get a random quote
-function getRandomQuote() {
-  fetch('https://api.quotable.io/random')
-  .then(response => response.json())
-  .then( async data => {
-    if (data.content.length >= 160) getRandomQuote();
-    const quote = await translateToFrench(data.content);
-    const {author} = data;
-    // Utilisation de la citation et de l'auteur
-    footer.innerHTML = `<h6><i>« ${quote} »</i><b> - ${author} </b></h6>`;
+// api to get a random advice
+function getRandomAdvice() {
+  fetch('https://api.adviceslip.com/advice')
+    .then((response) => response.json())
+    .then((data) => {
+      const {advice} = data.slip;
+    footer.innerHTML = `<h6><u> Advice</u>: <i> ${advice} </i></h6>`;
   })
   .catch( () => {
-    footer.innerHTML = `<h6><i>« L'amitié est un esprit en deux corps. »</i><b> - Mencius </b></h6>`;
+    footer.innerHTML = `<h6><u> Advice</u>: <i> Happiness is a journey, not a destination. </i></h6>`;
   });
 };
 
