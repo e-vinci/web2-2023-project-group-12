bb69a059b847b9c56aabbb050ec8d384c0d1ca1e
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-11-29 23:43:39 +0100

    add: GDPR

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -21,6 +21,12 @@ function renderRegisterForm() {
           <input class="bg-lavender form-control mb-3" type="password"  id="password" name="password" required>
           <label for="comfirm_password" class="form-label">Confirm password</label>
           <input class="bg-lavender form-control mb-2" type="password"  id="password" name="comfirm_password" required>
+          <div>
+            <input required="" type="checkbox" class="form-check-input">
+            <a href="https://policies.google.com/privacy?hl=en-US" class="link-warning link-underline-opacity-0 link-underline-opacity-100-hover text-white">
+                  Accept our policy
+            </a>
+          </div>
         </div>
 
         <button type="submit" class="btn btn-warning mt-3 col-8 rounded-5">Sign in</button>
