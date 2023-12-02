99341f389d2d96de67793997ee87fe40f334fb4e
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-12-02 18:21:07 +0100

    update: style and player on the game page

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -1,68 +1,44 @@
 import Phaser from 'phaser';
-import ScoreLabel from './ScoreLabel';
-import BombSpawner from './BombSpawner';
 import skyAsset from '../../assets/arena.png';
 import platformAsset from '../../assets/arena_platform.png';
-import starAsset from '../../assets/star.png';
-import bombAsset from '../../assets/bomb.png';
-import blueRobotAsset from '../../assets/blue-rrobot.png';
+import blueRobotAsset from '../../assets/blue-robot.png';
 import roseRobotAsset from '../../assets/rose-robot.png';
 
 
-const GROUND_KEY = 'ground';
 const BLUE_ROBOT_KEY = 'blue-robot';
 const ROSE_ROBOT_KEY = 'rose-robot';
-const STAR_KEY = 'star';
-const BOMB_KEY = 'bomb';
 
 class GameScene extends Phaser.Scene {
   constructor() {
     super('game-scene');
-    this.player = undefined;
     this.player2 = undefined;
+    this.player1 = undefined;
     this.cursors = undefined;
-    this.scoreLabel = undefined;
-    this.stars = undefined;
-    this.bombSpawner = undefined;
+    // this.scoreLabel = undefined;
     this.gameOver = false;
   }
 
   preload() {
     this.load.image('sky', skyAsset);
-    this.load.image(GROUND_KEY, platformAsset);
-    this.load.image(STAR_KEY, starAsset);
-    this.load.image(BOMB_KEY, bombAsset);
-    
+    this.load.image('ground', platformAsset);
     this.load.spritesheet(BLUE_ROBOT_KEY, blueRobotAsset, {
-      frameWidth: 82,
-      frameHeight: 80,
+      frameWidth: 91.55,
+      frameHeight: 90,
     });
-
     this.load.spritesheet(ROSE_ROBOT_KEY, roseRobotAsset, {
-      frameWidth: 83,
-      frameHeight: 80,
+      frameWidth: 93.25,
+      frameHeight: 90,
     });
   }
 
   create() {
-    this.add.image(401, 350, 'sky').setScale(0.4).setY(330).setX(601);
-    const platforms = this.createPlatforms();
-    this.player = this.createPlayer(100, 450, BLUE_ROBOT_KEY);
-    this.player2 = this.createPlayer(900, 450, ROSE_ROBOT_KEY);
-
-    this.stars = this.createStars();
-    this.scoreLabel = this.createScoreLabel(16, 16, 0);
-    this.bombSpawner = new BombSpawner(this, BOMB_KEY);
-    const bombsGroup = this.bombSpawner.group;
-    this.physics.add.collider(this.stars, platforms);
-    this.physics.add.collider(this.player, platforms);
-    this.physics.add.collider(this.player2, platforms);
-
-    this.physics.add.collider(bombsGroup, platforms);
-    this.physics.add.collider(this.player, bombsGroup, this.hitBomb, null, this);
-    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
-    this.physics.add.collider(this.player2, bombsGroup, this.hitBomb, null, this);
-    this.physics.add.overlap(this.player2, this.stars, this.collectStar, null, this);
+    this.add.image(401, 350, 'sky').setScale(0.4).setY(225).setX(600);
+    const platform = this.physics.add.staticGroup().create(600, 350, 'ground').setScale(0.4).setY(460).refreshBody();
+    this.player1 = this.createPlayer(100, 200, BLUE_ROBOT_KEY);
+    this.player2 = this.createPlayer(1100, 200, ROSE_ROBOT_KEY);
+    this.physics.add.collider(this.player2, platform);
+    this.physics.add.collider(this.player1, platform);
+    this.physics.add.collider(this.player1, this.player2);
     this.cursors = this.input.keyboard.createCursorKeys();
 
     /* The Collider takes two objects and tests for collision and performs separation against them.
@@ -74,56 +50,51 @@ class GameScene extends Phaser.Scene {
       return;
     }
 
+    if (this.physics.collide(this.player2, this.player1)) {
+      this.player2.setVelocity(0);
+    }
     // player 1 controls
     if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play('left-blue-robot', true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play('right-blue-robot', true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play('turn-blue-robot');
-    }
-  
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-300);
-    }
-  
-    if (this.cursors.down.isDown){
-      this.player.setVelocityY(300);
-    }
-  
-    // player 2 controls
-    if (this.input.keyboard.addKey('Q').isDown) {
       this.player2.setVelocityX(-160);
-      this.player2.anims.play('left-rose-robot', true);
-    } else if (this.input.keyboard.addKey('D').isDown) {
+      this.player2.anims.play('left-blue-robot', true);
+    } else if (this.cursors.right.isDown) {
       this.player2.setVelocityX(160);
-      this.player2.anims.play('right-rose-robot', true);
+      this.player2.anims.play('right-blue-robot', true);
     } else {
       this.player2.setVelocityX(0);
-      this.player2.anims.play('turn-rose-robot');
+      this.player2.anims.play('turn-blue-robot');
     }
   
-    if (this.input.keyboard.addKey('Z').isDown && this.player2.body.touching.down) {
+    if (this.cursors.up.isDown && this.player2.body.touching.down) {
       this.player2.setVelocityY(-300);
     }
   
-    if (this.input.keyboard.addKey('S').isDown){
+    if (this.cursors.down.isDown){
       this.player2.setVelocityY(300);
     }
-  }
-
-  createPlatforms() {
-    const platforms = this.physics.add.staticGroup();
-
-    platforms
-      .create(400, 568, GROUND_KEY)
-      .setScale(0.4)
-      .setX(600)
-      .refreshBody();
-    return platforms;
+  
+    if (this.physics.collide(this.player2, this.player1)) {
+      this.player1.setVelocity(0);
+    }
+    // player 2 controls
+    if (this.input.keyboard.addKey('Q').isDown) {
+      this.player1.setVelocityX(-160);
+      this.player1.anims.play('left-rose-robot', true);
+    } else if (this.input.keyboard.addKey('D').isDown) {
+      this.player1.setVelocityX(160);
+      this.player1.anims.play('right-rose-robot', true);
+    } else {
+      this.player1.setVelocityX(0);
+      this.player1.anims.play('turn-rose-robot');
+    }
+  
+    if (this.input.keyboard.addKey('Z').isDown && this.player1.body.touching.down) {
+      this.player1.setVelocityY(-300);
+    }
+  
+    if (this.input.keyboard.addKey('S').isDown){
+      this.player1.setVelocityY(300);
+    }
   }
 
   createPlayer(x, y, key) {
@@ -143,14 +114,12 @@ class GameScene extends Phaser.Scene {
       // eslint-disable-next-line prefer-template
       key: 'turn-' + key,
       // eslint-disable-next-line object-shorthand
-      frames: [{ key: key, frame: 4 }],
-      frameRate: 20,
+      frames: [{ key: key, frame: 3 }],
     });
 
     this.anims.create({
       key: 'turn-rose-robot',
-      frames: [{ key: ROSE_ROBOT_KEY, frame: 3 }],
-      frameRate: 20,
+      frames: [{ key: ROSE_ROBOT_KEY, frame: 4 }],
     });
   
     this.anims.create({
@@ -162,53 +131,6 @@ class GameScene extends Phaser.Scene {
     });
   
     return player;
-  }
-
-  createStars() {
-    const stars = this.physics.add.group({
-      key: STAR_KEY,
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 },
-    });
-
-    stars.children.iterate((child) => {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-
-    return stars;
-  }
-
-  collectStar(player, star) {
-    star.disableBody(true, true);
-    this.scoreLabel.add(10);
-    if (this.stars.countActive(true) === 0) {
-      //  A new batch of stars to collect
-      this.stars.children.iterate((child) => {
-        child.enableBody(true, child.x, 0, true, true);
-      });
-    }
-
-    this.bombSpawner.spawn(player.x);
-  }
-
-  createScoreLabel(x, y, score) {
-    const style = { fontSize: '32px', fill: '#000' };
-    const label = new ScoreLabel(this, x, y, score, style);
-    console.log('score:', label);
-    this.add.existing(label);
-
-    return label;
-  }
-
-  hitBomb(player) {
-    this.scoreLabel.setText(`GAME OVER : ( \nYour Score = ${this.scoreLabel.score}`);
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    this.gameOver = true;
   }
 }
 
--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -5,8 +5,11 @@ let game;
 
 const GamePage = () => {
   const phaserGame = `
-<div id="gameDiv" class="d-flex justify-content-center my-3">
-</div>`;
+  <div class="d-flex justify-content-center h-sm-75">
+    <div id="gameDiv" class="d-flex justify-content-center">
+    </div>
+  </div>
+`;
 
   const main = document.querySelector('main');
   main.innerHTML = phaserGame;
@@ -14,15 +17,15 @@ const GamePage = () => {
   const config = {
     type: Phaser.AUTO,
     width: 1200,
-    height: 600,
+    height: 500,
     physics: {
       default: 'arcade',
       arcade: {
-        gravity: { y: 500 },
+        gravity: { y: 1000 },
         debug: false,
       },
     },
-    canvasStyle: 'border: 4px solid #DAA520; box-sizing: border-box;', // Add any other styles as needed
+    canvasStyle: 'border: 7px solid #ffc107; box-sizing: border-box; border-radius: 10px;', // Add any other styles as needed
 
     scene: [GameScene],
     //  parent DOM element into which the canvas created by the renderer will be injected.
