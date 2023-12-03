2ba91da496995ace478073b45ef4b19ad9ad004d
melda-gonen-vinci <melda.gonen@student.vinci.be>
2023-12-03 19:06:01 +0100

    Résolution des conflits

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -1,9 +1,15 @@
+import { setAuthenticatedUser } from '../../utils/auths';
 import { clearPage } from '../../utils/render';
+import Navbar from '../Navbar/Navbar';
+import Navigate from '../Router/Navigate';
 
 
 const LoginPage = () => {
     clearPage();
     renderLoginForm();
+
+    const form = document.querySelector('main');
+    form.addEventListener('submit', onLogin);
   };
 
 function renderLoginForm() {
@@ -25,6 +31,58 @@ function renderLoginForm() {
       </form>
     </div>
     `;
+
+}
+
+async function onLogin(e) {
+
+  e.preventDefault();
+
+
+  const username = document.querySelector('#username').value;
+
+  const password = document.querySelector('#password').value;
+
+
+  const options = {
+
+    method: 'POST',
+
+    body: JSON.stringify({
+
+      username,
+
+      password,
+
+    }),
+
+    headers: {
+
+      'Content-Type': 'application/json',
+
+    },
+
+  };
+
+
+  const response = await fetch('/api/auths/login', options);
+
+
+  if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+
+
+  const authenticatedUser = await response.json();
+
+
+  console.log('Authenticated user : ', authenticatedUser);
+
+
+  setAuthenticatedUser(authenticatedUser);
+
+  Navbar()
+  
+  Navigate('/');
+
 }
 
 export default LoginPage;
