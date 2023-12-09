d4083600c7cb46f628915b8b2b5523d19463b2d7
Rekaya Nour <nourrkaya@live.be>
2023-12-09 21:45:11 +0100

    update: score-page user

--- frontend/src/Components/Pages/ScorePage.js
+++ frontend/src/Components/Pages/ScorePage.js
@@ -1,5 +1,8 @@
 import { clearPage } from '../../utils/render';
 
+import { getAuthenticatedUser} from '../../utils/auths';
+
+
 const ScorePage = () => {
     clearPage();
     renderScoreForm();
@@ -8,6 +11,9 @@ const ScorePage = () => {
 function renderScoreForm() {
 const main = document.querySelector('main');
 
+const authenticatedUser = getAuthenticatedUser()?.user;
+
+
 main.innerHTML = `
 <div class="container col-3 bg-black px-5 py-4 rounded-5 bg-opacity-50 text-center justify-content-center">
     <div class="bg-warning p-2 rounded mt-2 mb-4 fs-2 ">Scores</div>
@@ -15,19 +21,19 @@ main.innerHTML = `
         <div class="mt-5">
             <div class="d-flex justify-content-between fs-5">
                 <p class="col-sm-6 py-1 rounded-3 text-white">Username</p>
-                <p class="bg-lavender col-5 rounded-3">Ishou</p>
+                <p class="bg-lavender col-5 rounded-3">${authenticatedUser?.username}</p>
             </div>
             <div class="d-flex justify-content-between fs-5">
             <p class="col-sm-6 py-1 rounded-3 text-white">Games played</p>
-            <p class="bg-lavender col-5 rounded-3">100</p>
+            <p class="bg-lavender col-5 rounded-3">${authenticatedUser?.gamesPlayed}</p>
             </div>
             <div class="d-flex justify-content-between fs-5">
             <p class="col-sm-6 py-1 rounded-3 text-white">Games won</p>
-            <p class="bg-lavender col-5 rounded-3">0</p>
+            <p class="bg-lavender col-5 rounded-3">${authenticatedUser?.gamesWon}</p>
             </div>
             <div class="d-flex justify-content-between fs-5">
             <p class="col-sm-6 py-1 rounded-3 text-white">Games lost</p>
-            <p class="bg-lavender col-5 rounded-3">100</p>
+            <p class="bg-lavender col-5 rounded-3">${authenticatedUser?.gamesLost}</p>
         </div>
         </div>
     </div>
