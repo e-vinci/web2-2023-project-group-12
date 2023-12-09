6f10b2cec8f955776e7aa038acb6d5bfdfc9665a
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-12-09 19:49:30 +0100

    add: clear cookies user2

--- frontend/src/Components/Logout/Logout.js
+++ frontend/src/Components/Logout/Logout.js
@@ -1,9 +1,10 @@
-import { clearAuthenticatedUser } from '../../utils/auths';
+import { clearAuthenticatedUser, clearAuthenticatedUser2 } from '../../utils/auths';
 import Navbar from '../Navbar/Navbar';
 import Navigate from '../Router/Navigate';
 
 const Logout = () => {
   clearAuthenticatedUser();
+  clearAuthenticatedUser2();
   Navbar();
   Navigate('/');
 };
