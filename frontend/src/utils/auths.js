5c68713b89a00d7e13d0e05e94a5a1fc95b60b76
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-12-09 19:48:54 +0100

    add: cookies user1 & user2 until logout

--- frontend/src/utils/auths.js
+++ frontend/src/utils/auths.js
@@ -1,29 +1,65 @@
 let currentUser;
+const STORE_NAME = 'user';
+
 let secondUser;
+const STORE_NAME2 = 'user2';
 
-const getAuthenticatedUser = () => currentUser;
+const getAuthenticatedUser = () => {
+  if (currentUser !== undefined) return currentUser;
 
-const getAuthenticatedUser2 = () => secondUser;
+  const serializedUser = localStorage.getItem(STORE_NAME);
+
+  if (!serializedUser) return undefined;
+
+  currentUser = JSON.parse(serializedUser);
+
+  return currentUser;
+};
 
 const setAuthenticatedUser = (authenticatedUser) => {
+  const serializedUser = JSON.stringify(authenticatedUser);
+
+  localStorage.setItem(STORE_NAME, serializedUser);
+
   currentUser = authenticatedUser;
 };
 
-const setAuthenticatedUser2 = (authenticatedUser2) =>{
-  secondUser = authenticatedUser2;
-}
-
 const isAuthenticated = () => currentUser !== undefined;
 
-const isAuthenticated2 = () => secondUser !== undefined;
-
 const clearAuthenticatedUser = () => {
+
+  localStorage.removeItem(STORE_NAME);
   currentUser = undefined;
 };
 
+const getAuthenticatedUser2 = () => {
+  if (secondUser !== undefined) return secondUser;
+
+  const serializedUser = localStorage.getItem(STORE_NAME2);
+
+  if (!serializedUser) return undefined;
+
+  secondUser = JSON.parse(serializedUser);
+
+  return secondUser;
+};
+
+
+const setAuthenticatedUser2 = (authenticatedUser) => {
+  const serializedUser = JSON.stringify(authenticatedUser);
+
+  localStorage.setItem(STORE_NAME2, serializedUser);
+
+  secondUser = authenticatedUser;
+};
+
+const isAuthenticated2 = () => secondUser !== undefined;
+
 const clearAuthenticatedUser2 = () => {
+
+  localStorage.removeItem(STORE_NAME2);
   secondUser = undefined;
-}
+};
 
 // eslint-disable-next-line object-curly-newline
-export { getAuthenticatedUser, setAuthenticatedUser, isAuthenticated, clearAuthenticatedUser, setAuthenticatedUser2, clearAuthenticatedUser2, getAuthenticatedUser2, isAuthenticated2 };
+export { getAuthenticatedUser, setAuthenticatedUser, isAuthenticated, clearAuthenticatedUser, getAuthenticatedUser2, setAuthenticatedUser2, isAuthenticated2, clearAuthenticatedUser2 };
