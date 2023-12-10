c0a5d49991e85f9cc66f9ffd129c517627f07564
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-12-10 20:12:20 +0100

    update: cors origin

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const cookieSession = require('cookie-session');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'https://e-baron.github.io'],
+  origin: ['http://localhost:8080', 'https://e-vinci.github.io/SpaceLover'],
 };
 
 const usersRouter = require('./routes/users');
