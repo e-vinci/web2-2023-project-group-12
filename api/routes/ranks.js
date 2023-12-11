49d9cb24abd29ef21696273c9e051062f7e3c3c0
Vanya-Yahya-vinci <vanya.caylanadi@student.vinci.be>
2023-12-11 14:37:44 +0100

    api : ranking

---
+++ api/routes/ranks.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { readAllRanking } = require('../models/users');
+
+const router = express.Router();
+
+// rank page
+router.get('/', (req, res) => {
+  console.log("hhaha");
+  const ranking = readAllRanking();
+  return res.json(ranking);
+});
+
+module.exports = router;
