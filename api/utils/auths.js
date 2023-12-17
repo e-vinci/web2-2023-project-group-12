5f812633fe27b601e4d90fc24016591005e46406
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-12-17 18:00:01 +0100

    comment: disable console with lint

--- api/utils/auths.js
+++ api/utils/auths.js
@@ -18,6 +18,7 @@ const authorize = (req, res, next) => {
     req.user = existingUser; // request.user object is available in all other middleware functions
     return next();
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.error('authorize: ', err);
     return res.sendStatus(401);
   }
